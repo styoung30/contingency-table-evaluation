b346a1ad70c0d393764a21653fcb0d3d60b463b4
Shawn Young <styoung306@gmail.com>
2015-03-16 23:59:30 -0400

    fixed some typos

--- mevaluate.py
+++ mevaluate.py
@@ -18,12 +18,20 @@ def mevaluate(Model_Name, Tn, Fn, Fp, Tp):
     print("Proportion of True Positives:"), Tp / (Fp + Tp)
     print("Proportion of True Negatives:"), Tn / (Fn + Tn)
     print("Proportion of False Positives:"), 1 - (Tp / (Fp + Tp))
-    print("Proportion of True Negatives:"), 1 - (Tn / (Fn + Tn))
+    print("Proportion of False Negatives:"), 1 - (Tn / (Fn + Tn))
     print("----------------------------------------------------")
 """
 Call function mevaluate by replacing the existing input in the format:
 mevaluate("Model Name", Tn.0, Fn.0, Fp.0, Tp.0)
 """
 
-mevaluate("With Interest", 15624.0, 14823.0, 9310.0, 9941.0)
+
 mevaluate("No Interest", 13730.0, 743.0, 11204.0, 24021.0)
+mevaluate("RS1", 10462.0, 5.0, 14472.0, 24759.0)
+mevaluate("RS2", 18263.0, 7116.0, 6671.0, 17648.0)
+mevaluate("RS3", 12315.0, 201.0, 12619.0, 24563.0)
+mevaluate("RS4", 19692.0, 4603.0, 5242.0, 20161.0)
+
+
+
+
